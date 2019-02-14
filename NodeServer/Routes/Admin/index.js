a341b5dd6cf7c4788edab302ee3b0a005ba3520d
leviM <levim770@gmail.com>
2019-02-14 15:41:03 +0200

    aoc...

--- NodeServer/Routes/Admin/index.js
+++ NodeServer/Routes/Admin/index.js
@@ -4,29 +4,20 @@ const repo = require('../../Dal/Repository');
 const sqlP = require('../../Models/SqlParameter');
 const sql = require('mssql');
 
-const AuthProcedure = require('../../Dal/Auth/AuthProcedure')
 router.post('/Login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     let p = new sqlP.SqlParmaeter();
     if (email && password) {
-        let params = [p.getAsSQLParameter('email', sql.VarChar(50), email),
-                      p.getAsSQLParameter('password', sql.VarChar(50), password)]
+        let params = [new sqlP.SqlParmaeter('email', sql.VarChar(50), email),
+                      new sqlP.SqlParmaeter('password', sql.VarChar(50), password)]
         repo.WriteInDb('sp_login', params, user => {
             console.log(user);
             res.send(user).status(200);
         })
-
-        let user = AuthProcedure.login(email, password);
-        console.log(user);
-        res.send(user).status(200);
     }
     else {
         res.status(400).send('missing username or password');
     }
-});
-
-router.post('/Logout', (req, res) => {
-   const email = req.body.email;
 });
 module.exports = router;
