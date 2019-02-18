1f09504555dd1a3182f854dcb853ae1f118ad985
ItamarDaisy <itamardaisy@gmail.com>
2019-02-18 14:50:21 +0200

    Change o lot of things

--- Client/src/app/Services/Register/register.service.ts
+++ Client/src/app/Services/Register/register.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Admin } from 'src/app/Models/AdminUser';
 import { Observable } from 'rxjs';
-import { constantFields } from '../../../../../../Test_System/constantFields.js';
 
 @Injectable({
     providedIn: 'root'
@@ -11,6 +10,6 @@ export class RegisterService {
 
     constructor(private http: HttpClient) { }
     public register(newAdmin: Admin): Observable<Admin> {
-        return this.http.post<Admin>(`http://localhost:${constantFields.SERVER_PORT}/admin/register`, newAdmin);
+        return this.http.post<Admin>(`http://localhost:5221/admin/register`, newAdmin);
     }
 }
