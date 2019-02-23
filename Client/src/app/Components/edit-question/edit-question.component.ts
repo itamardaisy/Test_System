166785a33c9054eb10a876b6d61db3e9b288d460
ItamarDaisy <itamardaisy@gmail.com>
2019-02-24 00:04:43 +0200

    craete the add question button

--- Client/src/app/Components/edit-question/edit-question.component.ts
+++ Client/src/app/Components/edit-question/edit-question.component.ts
@@ -2,25 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Answer } from 'src/app/Models/Answer';
 
 @Component({
-  selector: 'app-edit-question',
-  templateUrl: './edit-question.component.html',
-  styleUrls: ['./edit-question.component.css']
+    selector: 'app-edit-question',
+    templateUrl: './edit-question.component.html',
+    styleUrls: ['./edit-question.component.css']
 })
 export class EditQuestionComponent implements OnInit {
-  options = ['Single answer', 'Two Answers', 'Three answers'];
-  answers: Answer[];
-  answer: Answer;
-  constructor() {}
+    public options = ['Single answer', 'Two Answers', 'Three answers'];
+    public answers: Answer[] = new Array();
+    public textEmpty: boolean;
+    answersNotEmpty: boolean;
 
-  ngOnInit() {
-    this.answer = new Answer('', false);
-  }
+    constructor() {}
 
-  addQuestion() {
+    ngOnInit() {}
 
-  }
+    addQuestion() {
+        if (this.answers.length === 0) {
+            this.answersNotEmpty = true;
+        }
+        console.log(`${this.answers.length} kojewrpjgfiosadngfuiasde`);
+        const numOfQuestions = this.answers.length + 1;
+        this.answers.push(new Answer(`Question: ${numOfQuestions}`, false));
+    }
 
-  changeState(event) {
-    this.answer.IsCorrect = event.target.checked;
-  }
+    changeState(event) {}
 }
